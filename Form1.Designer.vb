aea7e8633bd6865cd4e5874e25b9aa0ad0dce50a
UABJB <39167261+UABJB@users.noreply.github.com>
2018-05-10 20:02:38 +0200

    Commit BLADIMIR
    Modificando btnRegistro y btnSalir

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,102 +23,113 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btnregistro = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtAPM = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtAPP = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(48, 37)
+        Me.Label1.Location = New System.Drawing.Point(48, 36)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Nombre"
         '
-        'Button1
+        'btnregistro
         '
-        Me.Button1.Location = New System.Drawing.Point(106, 143)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnregistro.Location = New System.Drawing.Point(106, 143)
+        Me.btnregistro.Name = "btnregistro"
+        Me.btnregistro.Size = New System.Drawing.Size(75, 23)
+        Me.btnregistro.TabIndex = 1
+        Me.btnregistro.Text = "Registro"
+        Me.btnregistro.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtNombre
         '
-        Me.TextBox1.Location = New System.Drawing.Point(106, 29)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 2
+        Me.txtNombre.Location = New System.Drawing.Point(106, 29)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 2
         '
-        'TextBox2
+        'txtAPM
         '
-        Me.TextBox2.Location = New System.Drawing.Point(106, 55)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 4
+        Me.txtAPM.Location = New System.Drawing.Point(106, 55)
+        Me.txtAPM.Name = "txtAPM"
+        Me.txtAPM.Size = New System.Drawing.Size(100, 20)
+        Me.txtAPM.TabIndex = 4
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(48, 63)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
         Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "APM"
         '
-        'TextBox3
+        'txtAPP
         '
-        Me.TextBox3.Location = New System.Drawing.Point(106, 81)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 6
+        Me.txtAPP.Location = New System.Drawing.Point(106, 81)
+        Me.txtAPP.Name = "txtAPP"
+        Me.txtAPP.Size = New System.Drawing.Size(100, 20)
+        Me.txtAPP.TabIndex = 6
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Location = New System.Drawing.Point(48, 89)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.Size = New System.Drawing.Size(28, 13)
         Me.Label3.TabIndex = 5
-        Me.Label3.Text = "Label3"
+        Me.Label3.Text = "APP"
         '
-        'TextBox4
+        'txtEdad
         '
-        Me.TextBox4.Location = New System.Drawing.Point(106, 107)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox4.TabIndex = 8
+        Me.txtEdad.Location = New System.Drawing.Point(106, 107)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(100, 20)
+        Me.txtEdad.TabIndex = 8
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Location = New System.Drawing.Point(48, 115)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.Size = New System.Drawing.Size(32, 13)
         Me.Label4.TabIndex = 7
-        Me.Label4.Text = "Label4"
+        Me.Label4.Text = "Edad"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(214, 161)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(26, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "X"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(252, 196)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtEdad)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtAPP)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtAPM)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.btnregistro)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -128,12 +139,13 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents btnregistro As Button
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtAPM As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtAPP As TextBox
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents txtEdad As TextBox
     Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
+    Private Sub btnregistro_Click(sender As Object, e As EventArgs) Handles btnregistro.Click
+        'REgistro de persona
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+
+    End Sub
 End Class
